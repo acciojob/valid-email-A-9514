de6e8125eece0d0167b6d7579d045c4c87b8f9e2
Aishwarya <142329750+A-9514@users.noreply.github.com>
2023-12-17 20:22:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function validEmail(str) {
   //your JS code here.
+	return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
 }
 
 // Do not change the code below.
